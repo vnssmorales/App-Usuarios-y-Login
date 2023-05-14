7edea4baf43697c028f7e7b74660b97b0d8b40ca
vanemh <vnss.morales@gmail.com>
2023-05-14 13:26:26 -0400

    implementando vista de login

--- src/UsersApp.jsx
+++ src/UsersApp.jsx
@@ -1,60 +1,11 @@
-import { UserForm } from "./components/UserForm";
-import { UserModalForm } from "./components/UserModalForm";
-import { UsersList } from "./components/UsersList";
-import { useUsers } from "./hooks/useUsers";
+import { LoginPage } from "./auth/pages/LoginPage";
+import { UsersPage } from "./pages/UsersPage";
 
 export const UsersApp = () => {
-  const {
-    users,
-    userSelected,
-    initialUserForm,
-    visibleForm,
-
-    handlerAddUser,
-    handlerRemoveUser,
-    handlerUserSelectedForm,
-    handlerOpenForm,
-    handlerCloseForm,
-  } = useUsers();
-
   return (
     <>
-      {!visibleForm || 
-        <UserModalForm 
-        userSelected ={userSelected}
-        initialUserForm={initialUserForm}
-        handlerAddUser={handlerAddUser}
-        handlerCloseForm={handlerCloseForm}
-        />
-        }
-
-      <div className="container my-4">
-        <h2>Users App</h2>
-        <div className="row">
-          <div className="col">
-            {visibleForm || (
-              <button
-                className="btn btn-primary my-2"
-                onClick={handlerOpenForm}
-              >
-                Nuevo usuario
-              </button>
-            )}
-
-            {users.length === 0 ? (
-              <div className="alert alert-warning">
-                No hay usuarios en el sistema
-              </div>
-            ) : (
-              <UsersList
-                handlerUserSelectedForm={handlerUserSelectedForm}
-                handlerRemoveUser={handlerRemoveUser}
-                users={users}
-              />
-            )}
-          </div>
-        </div>
-      </div>
+      <LoginPage />
+      {/* <UsersPage/> */}
     </>
   );
 };
